78eb98e763231b5605015973acc57ee43e2228f8
JeanSegat <segat.jean@gmail.com>
2021-06-26 15:20:35 +0200

    course

---
+++ src/main/java/guru/springframework/Sum.java
@@ -0,0 +1,27 @@
+package guru.springframework;
+
+public class Sum implements Expression {
+    Expression augment;
+    Expression addend;
+
+    public Sum(Expression augment, Expression addend) {
+        this.augment = augment;
+        this.addend = addend;
+    }
+
+    @Override
+    public Money reduce(Bank bank, String to) {
+        int amount = augment.reduce(bank, to).amount + addend.reduce(bank, to).amount;
+        return new Money(amount,to);
+    }
+
+    @Override
+    public Expression plus(Expression addend) {
+        return new Sum(this, addend);
+    }
+
+    @Override
+    public Expression times(int multiplier) {
+        return new Sum(augment.times(multiplier), addend.times(multiplier));
+    }
+}
